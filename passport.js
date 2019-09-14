c3b9dae55758e7c027561a2de8a085d0c29909af
trohini31383 <53841712+trohini31383@users.noreply.github.com>
2019-09-14 15:47:27 -0700

    Create passport.js

---
+++ passport.js
@@ -0,0 +1,50 @@
+const passport = require("passport"),
+  LocalStrategy = require("passport-local").Strategy,
+  Models = require("./models.js"),
+  passportJWT = require("passport-jwt");
+var Users = Models.User;
+var JWTStrategy = passportJWT.Strategy;
+var ExtractJWT = passportJWT.ExtractJwt;
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "Name",
+      passwordField: "Password"
+    },
+    (name, password, callback) => {
+      console.log(name + "  " + password);
+      Users.findOne({ Name: name }, (error, user) => {
+        if (error) {
+          console.log(error);
+          return callback(error);
+        }
+        if (!user) {
+          console.log("incorrect username");
+          return callback(null, false, {
+            message: "Incorrect username or password."
+          });
+        }
+        console.log("finished");
+        return callback(null, user);
+      });
+    }
+  )
+);
+
+passport.use(
+  new JWTStrategy(
+    {
+      jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+      secretOrKey: "your_jwt_secret"
+    },
+    (jwtPayload, callback) => {
+      return Users.findById(jwtPayload._id)
+        .then(user => {
+          return callback(null, user);
+        })
+        .catch(error => {
+          return callback(error);
+        });
+    }
+  )
+);
