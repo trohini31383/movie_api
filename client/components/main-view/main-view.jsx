9c69a0a7eb9c3197880698307c8ea65aa0c1da1e
trohini31383 <rohini.tomar@gmail.com>
2019-09-24 06:53:46 -0700

    updated client

---
+++ client/components/main-view/main-view.jsx
@@ -0,0 +1,34 @@
+import React from react;
+import axios from axios;
+export class MainView extends React.Component {
+
+
+  componentDidMount() {
+    axios.get('<my-api-endpoint/movies>')
+      .then(response => {
+        this.setState({
+          movies: response.data
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+
+  render() {
+
+    const { movies } = this.state;
+
+
+    if (!movies) return <div className="main-view" />;
+
+    return (
+      <div className="main-view">
+        {movies.map(movie => (
+          <div className="movie-card" key={movie._id}>{movie.Title}</div>
+        ))}
+      </div>
+    );
+  }
+}
