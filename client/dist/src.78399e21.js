6366d0850f1f23053ec403d3fdc525b1a09eaeee
trohini31383 <rohini.tomar@gmail.com>
2019-10-15 21:34:56 -0700

    updated index.js and mainview

--- client/dist/src.78399e21.js
+++ client/dist/src.78399e21.js
@@ -39715,7 +39715,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60894" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61263" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
