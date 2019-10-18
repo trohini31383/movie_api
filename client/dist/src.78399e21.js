b81cff0e31974726ba7fb92677226132ad31b339
trohini31383 <rohini.tomar@gmail.com>
2019-10-18 10:54:57 -0700

    updated index.js

--- client/dist/src.78399e21.js
+++ client/dist/src.78399e21.js
@@ -39715,7 +39715,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61263" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51426" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
