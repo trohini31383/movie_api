f32d2be37b26a4285d66faaa7f00a1cd819492b5
trohini31383 <rohini.tomar@gmail.com>
2020-01-05 09:45:50 -0800

    added redux

--- index.js
+++ index.js
@@ -288,9 +288,9 @@ app.get(
   }
 );
 app.use(express.static("public"));
-app.use('/client', express.static(path.join(__dirname, 'dist')));
+app.use('/client', express.static(path.join(__dirname, 'client/dist')));
 app.get("/client/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "client/dist", "index.html"));
 });
 app.use(function (err, req, res, next) {
   console.error(err.stack);
