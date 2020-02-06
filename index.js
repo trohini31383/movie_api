2d7cee38a94b531d1ce31f5b1534d01ae8ba2c33
trohini31383 <rohini.tomar@gmail.com>
2020-02-06 13:43:55 -0800

    updated index.js for checking heroku

--- index.js
+++ index.js
@@ -287,10 +287,15 @@ app.get(
       });
   }
 );
-app.use(express.static("public"));
+/*app.use(express.static("public"));
 app.use('/client', express.static(path.join(__dirname, 'client/dist')));
 app.get("/client/*", (req, res) => {
   res.sendFile(path.join(__dirname, "client/dist", "index.html"));
+});*/
+app.use(express.static("public"));
+app.use("/client", express.static(path.join(__dirname, "client", "dist")));
+app.get("/client/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
 app.use(function (err, req, res, next) {
   console.error(err.stack);
